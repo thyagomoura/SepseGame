92e4b89604d5e0f1e55611f7487989d357548ebf
Thyago Moura <61210307+thyagomoura@users.noreply.github.com>
2022-01-23 11:52:47 -0300

    Acrescentado o main menu e o pause menu
    Foram criados e implementados os menus funcionais, faltando apenas estilização

---
+++ SepseGame/Assets/Scripts/Menu/MenuMain.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuMain : MonoBehaviour
+{
+    public static bool GameIsOption = false;
+    public GameObject OptionsMenuUI;
+    public GameObject MainMenuUI;
+
+    public void Play()
+    {
+        //ir pro jogo
+        SceneManager.LoadScene("Dialogo");
+    }
+    public void Quit()
+    {
+        //sair do jogo
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+    public void GoOptionMenu()
+    {
+        OptionsMenuUI.SetActive(true);
+        MainMenuUI.SetActive(false);
+    }
+
+    public void BackMainMenu()
+    {
+        OptionsMenuUI.SetActive(false);
+        MainMenuUI.SetActive(true);
+        //sair do menu options e voltar para o main menu
+        Debug.Log("Voltar");
+    }
+}
---
+++ SepseGame/Assets/Scripts/Menu/MenuPause.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuPause : MonoBehaviour
+{
+
+    public GameObject PauseMenuUI;
+    public GameObject PauseIcon;
+
+
+    public void Resume()
+    {
+        PauseIcon.SetActive(true);
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        PauseIcon.SetActive(false);
+        Time.timeScale = 0f;
+    }
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
