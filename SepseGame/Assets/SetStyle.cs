8e48243ce58567b634ace4d469118ac9b4df4d9d
Thyago Moura <61210307+thyagomoura@users.noreply.github.com>
2022-06-06 22:59:38 -0300

    Revert ".."
    This reverts commit 3d6819a4b46e0ca112895b6d0292fb37c2704a9c.

---
+++ SepseGame/Assets/SetStyle.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetStyle : MonoBehaviour
+{
+    public EsteticaNurse en;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
