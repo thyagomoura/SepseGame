8721736441205afa0a299f359ec7ca6e58975651
Thyago Moura <61210307+thyagomoura@users.noreply.github.com>
2022-07-21 19:41:21 -0300

    LOGIN: Script de login atualizado para sempre receber valores no nome e cpf

--- SepseGame/Assets/Scripts/Login/Login.cs
+++ SepseGame/Assets/Scripts/Login/Login.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Text.RegularExpressions;
 
 public class Login : MonoBehaviour
 {
@@ -11,6 +12,9 @@ public class Login : MonoBehaviour
     public static string cpf = "12345678910";
     public GameObject inputFieldNome;
     public GameObject inputFieldCpf;
+    public GameObject PlaceHolderNome;
+    public GameObject PlaceHolderCpf;
+    public bool permited = false;
 
     public void ReceberNome()
     {
@@ -20,11 +24,37 @@ public class Login : MonoBehaviour
     public void ReceberCpf()
     {
         cpf = inputFieldCpf.GetComponent<TextMeshProUGUI>().text;
-        
     }
+
     [System.Obsolete]
     public void ProxScene()
     {
-        Application.LoadLevel("TextoInicial");
+        if (nome != "defaultName")
+        {
+            permited = true;
+        }
+        else
+        {
+            permited = false;
+            PlaceHolderNome.GetComponent<TextMeshProUGUI>().text = "O nome ainda não foi inserido!";
+            PlaceHolderNome.GetComponent<TextMeshProUGUI>().color = Color.red;
+        }
+        if(permited)
+        {
+            if (cpf != "12345678910")
+            {
+                permited = true;
+            }
+            else
+            {
+                permited = false;
+                PlaceHolderCpf.GetComponent<TextMeshProUGUI>().text = "O CPF ainda não foi inserido";
+                PlaceHolderCpf.GetComponent<TextMeshProUGUI>().color = Color.red;
+            }
+        }
+        if (permited)
+        {
+            Application.LoadLevel("TextoInicial");
+        } 
     }
 }
