f35750c42348bcfa7c0e5d556b7cb012699344ac
Thyago Moura <61210307+thyagomoura@users.noreply.github.com>
2022-07-21 19:05:17 -0300

    Analise jogo, faltam 2 pontos para arrumar

--- SepseGame/Assets/Scripts/Menu/Text1Handler.cs
+++ SepseGame/Assets/Scripts/Menu/Text1Handler.cs
@@ -6,7 +6,11 @@ using TMPro;
 public class Text1Handler : MonoBehaviour
 {
     int currentPosition;
-    string fullText = "Você será o (a) enfermeiro (a) que está de plantão na unidade de emergência e deverá avaliar cada paciente admitido. O desafio de hoje é reconhecer os casos confirmados ou suspeitos de sepse e com base na presença de disfunção orgânica ou critérios de síndrome de resposta inflamatória sistêmica (SRIS) deverá tomar decisões acerca da abertura  ou não do protocolo de sepse, chamar ou não a equipe médica, iniciar ou não o pacote de primeira hora para o tratamento de casos suspeitos, priorizar as condutas de enfermagem necessárias para cada caso clínico.";
+    string fullText = "Você será o (a) enfermeiro (a) que está de plantão na unidade de emergência e deverá avaliar cada paciente admitido. O desafio de hoje é reconhecer" +
+        " os casos confirmados ou suspeitos de sepse e  com base na presença de disfunção orgânica ou critérios de síndrome de resposta inflamatória sistêmica (SRIS) deverá " +
+        "tomar decisões acerca da abertura  ou não do protocolo de sepse, chamar ou não a equipe médica, iniciar ou não o pacote de primeira hora para o tratamento de casos" +
+        " suspeitos, priorizar as condutas de enfermagem necessárias para cada caso clínico.";
+
     float Delay = 0.04f;
     public GameObject T1;
 
