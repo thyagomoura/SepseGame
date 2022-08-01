3b84c089db0e69b98da72e1be3f3afe9930ed9e3
Thyago Moura <61210307+thyagomoura@users.noreply.github.com>
2022-08-01 19:01:36 -0300

    inserido tempo final antes de fechar o jogo

--- SepseGame/Assets/Scripts/Managers/FInalSceneManager.cs
+++ SepseGame/Assets/Scripts/Managers/FInalSceneManager.cs
@@ -8,11 +8,11 @@ public class FInalSceneManager : MonoBehaviour
 {
     public GameObject ptsTxt;
     public List<Caso> Casos;
-
     int totalPontos;
 
     private void Start()
     {
+        
         /*int i;
         for (i=0; i < Casos.Count; i++)
         {
@@ -21,8 +21,17 @@ public class FInalSceneManager : MonoBehaviour
 
         ptsTxt.GetComponent<TextMeshProUGUI>().text = ptsTxt.GetComponent<TextMeshProUGUI>().text + totalPontos.ToString() + " pontos";*/
     }
+    IEnumerator Example()
+    {
+        yield return new WaitForSeconds(45);
+    }
 
     public void fechar()
+    {
+        StartCoroutine(Example());
+        fechando();
+    }
+    public void fechando()
     {
         Application.Quit();
     }
